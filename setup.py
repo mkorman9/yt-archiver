60fb329227354cd17fb715083bb6941545780586
mkorman9 <michal.korman@icloud.com>
2018-06-02 11:51:16 +0200

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE.txt') as f:
 
 setup(
     name='yt-archiver',
-    version='0.1.0',
+    version='0.2.0',
     description='Daemon for monitoring YouTube channels for new videos. Offers automatic backup of uploaded content',
     long_description=readme,
     license=license,
