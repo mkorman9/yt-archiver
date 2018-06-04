a62ca609336f58ac392a4cfed4da009bea025739
mkorman <michal.korman@icloud.com>
2018-06-04 15:41:41 +0200

    catch all exception during channel content lookup

--- ytarchiver/lookup.py
+++ ytarchiver/lookup.py
@@ -8,7 +8,66 @@ from ytarchiver.download import generate_livestream_filename, generate_video_fil
 from ytarchiver.storage import open_storage, Storage
 
 
-class Statistics:
+def lookup(context: Context, is_first_run: bool):
+    statistics = _Statistics(
+        is_first_run=is_first_run,
+        monitor_livestreams=context.config.monitor_livestreams
+    )
+
+    context.livestream_recorders.update(context)
+    context.video_recorders.update(context)
+
+    with open_storage(context.config.output_dir) as storage:
+        for channel_id in context.config.channels_list:
+            _fetch_channel_content(context, channel_id, storage, statistics, is_first_run)
+            storage.commit()
+
+    statistics.announce(context.logger)
+
+
+def _fetch_channel_content(context: Context, channel_id: str, storage: Storage, statistics, is_first_run: bool=False):
+    try:
+        _check_for_livestreams(context, channel_id, statistics, storage)
+        _check_for_videos(context, channel_id, is_first_run, statistics, storage)
+    except ConnectionError as e:
+        context.logger.error('connection to API has failed, skipping lookup')
+        context.logger.error(e)
+    except HttpError as e:
+        context.logger.error('API call has failed, skipping lookup')
+        context.logger.error(e)
+    except Exception as e:
+        context.logger.error('unknown error')
+        context.logger.error(e)
+
+
+def _check_for_livestreams(context: Context, channel_id: str, statistics: '_Statistics', storage: Storage):
+    if not context.config.monitor_livestreams:
+        return
+
+    livestream = fetch_channel_livestream(context, channel_id)
+    if livestream is not None:
+        if not context.livestream_recorders.is_recording_active(livestream.video_id):
+            livestream.filename = generate_livestream_filename(context.config.output_dir, livestream)
+            context.logger.info('new livestream "{}"'.format(livestream.title))
+            context.livestream_recorders.start_recording(context, livestream)
+            storage.add_livestream(livestream)
+        statistics.notify_active_livestream()
+
+
+def _check_for_videos(context: Context, channel_id: str, is_first_run: bool, statistics: '_Statistics', storage: Storage):
+    for video in find_channel_uploaded_videos(context, channel_id, is_first_run):
+        video_not_registered = not storage.video_exist(video.video_id) and \
+                               not context.video_recorders.is_recording_active(video.video_id)
+        if video_not_registered:
+            video.filename = generate_video_filename(context.config.output_dir, video)
+            context.logger.info('new video "{}"'.format(video.title))
+            if not is_first_run or context.config.archive_all:
+                context.video_recorders.start_recording(context, video)
+            storage.add_video(video)
+        statistics.notify_video(new=video_not_registered)
+
+
+class _Statistics:
     def __init__(self, is_first_run: bool, monitor_livestreams: bool):
         self.is_first_run = is_first_run
         self.monitor_livestreams = monitor_livestreams
@@ -33,62 +92,3 @@ class Statistics:
                 self.active_livestreams if self.monitor_livestreams else 'N/A'
             )
         )
-
-
-def lookup(context: Context, is_first_run: bool):
-    statistics = Statistics(
-        is_first_run=is_first_run,
-        monitor_livestreams=context.config.monitor_livestreams
-    )
-
-    context.livestream_recorders.update(context)
-    context.video_recorders.update(context)
-
-    with open_storage(context.config.output_dir) as storage:
-        for channel_id in context.config.channels_list:
-            _fetch_channel_content(context, channel_id, storage, statistics, is_first_run)
-
-    statistics.announce(context.logger)
-
-
-def _fetch_channel_content(context: Context, channel_id: str, storage: Storage, statistics, is_first_run: bool=False):
-    try:
-        _check_for_livestreams(context, channel_id, statistics, storage)
-        _check_for_videos(context, channel_id, is_first_run, statistics, storage)
-    except ConnectionError as e:
-        context.logger.error('connection to API has failed, skipping lookup')
-        context.logger.error(e)
-        return
-    except HttpError as e:
-        context.logger.error('API call has failed, skipping lookup')
-        context.logger.error(e)
-        return
-    finally:
-        storage.commit()
-
-
-def _check_for_livestreams(context: Context, channel_id: str, statistics: Statistics, storage: Storage):
-    if not context.config.monitor_livestreams:
-        return
-
-    livestream = fetch_channel_livestream(context, channel_id)
-    if livestream is not None:
-        if not context.livestream_recorders.is_recording_active(livestream.video_id):
-            livestream.filename = generate_livestream_filename(context.config.output_dir, livestream)
-            context.logger.info('new livestream "{}"'.format(livestream.title))
-            context.livestream_recorders.start_recording(context, livestream)
-            storage.add_livestream(livestream)
-        statistics.notify_active_livestream()
-
-
-def _check_for_videos(context: Context, channel_id: str, is_first_run: bool, statistics: Statistics, storage: Storage):
-    for video in find_channel_uploaded_videos(context, channel_id, is_first_run):
-        video_not_registered = not storage.video_exist(video.video_id) and \
-                               not context.video_recorders.is_recording_active(video.video_id)
-        if video_not_registered:
-            video.filename = generate_video_filename(context.config.output_dir, video)
-            context.logger.info('new video "{}"'.format(video.title))
-            if not is_first_run or context.config.archive_all:
-                context.video_recorders.start_recording(context, video)
-            storage.add_video(video)
-        statistics.notify_video(new=video_not_registered)
